00d945c8486fef85b1f36c7ff87e6d481030e50f
Nawaf Nazeer <59618797+unnamed-catalyst@users.noreply.github.com>
2021-08-02 15:26:49 +0300

    Update Hacking.py

--- Hacking.py
+++ Hacking.py
@@ -1,4 +1,4 @@
-# Hacking Version
+# Hacking
 # This is a text-based password guessing game that displays a
 # list of potential computer passwords. The player is allowed
 # 1 attempt to guess the password. The game indicates that the
